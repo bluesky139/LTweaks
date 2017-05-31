1153b2dcfcc6968118aa93ff729652d7b1d43cd1
bluesky139 <bluesky139@gmail.com>
2017-05-31 22:58:43 +0800

    [Douban Movie] Open link in app.

---
+++ app/src/main/java/li/lingfeng/ltweaks/xposed/entertainment/XposedDoubanMovieUrl.java
@@ -0,0 +1,45 @@
+package li.lingfeng.ltweaks.xposed.entertainment;
+
+import android.content.IntentFilter;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageParser;
+
+import java.lang.reflect.Field;
+
+import de.robv.android.xposed.XC_MethodHook;
+import li.lingfeng.ltweaks.R;
+import li.lingfeng.ltweaks.lib.XposedLoad;
+import li.lingfeng.ltweaks.prefs.ClassNames;
+import li.lingfeng.ltweaks.prefs.PackageNames;
+import li.lingfeng.ltweaks.utils.Logger;
+import li.lingfeng.ltweaks.xposed.XposedBase;
+
+/**
+ * Created by smallville on 2017/5/31.
+ */
+@XposedLoad(packages = PackageNames.ANDROID, prefs = R.string.key_douban_movie_url)
+public class XposedDoubanMovieUrl extends XposedBase {
+    @Override
+    protected void handleLoadPackage() throws Throwable {
+        hookAllMethods(ClassNames.PACKAGE_PARSER, "parseActivity", new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                PackageParser.Package owner = (PackageParser.Package) param.args[0];
+                if (!owner.packageName.equals(PackageNames.DOUBAN_MOVIE)) {
+                    return;
+                }
+
+                PackageParser.Activity activity = (PackageParser.Activity) param.getResult();
+                Field fieldInfo = PackageParser.Activity.class.getDeclaredField("info");
+                fieldInfo.setAccessible(true);
+                ActivityInfo info = (ActivityInfo) fieldInfo.get(activity);
+                if (!info.name.equals("com.douban.movie.activity.InnerFacadeActivity")) {
+                    return;
+                }
+
+                Logger.i("Set Douban Movie activity exported to true.");
+                info.exported = true;
+            }
+        });
+    }
+}
