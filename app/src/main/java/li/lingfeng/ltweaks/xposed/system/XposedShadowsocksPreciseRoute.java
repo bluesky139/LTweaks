d220ddeea15f16fae48eb98c3dd44e307a6865dc
bluesky139 <bluesky139@gmail.com>
2017-05-31 19:52:22 +0800

    [Shadowsocks] Use precise route, for "Bypass LAN & mainland China" only.

--- app/src/main/java/li/lingfeng/ltweaks/xposed/system/XposedShadowsocksPreciseRoute.java
+++ app/src/main/java/li/lingfeng/ltweaks/xposed/system/XposedShadowsocksPreciseRoute.java
@@ -3,6 +3,8 @@ package li.lingfeng.ltweaks.xposed.system;
 import android.content.Context;
 import android.content.res.Resources;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Set;
 
@@ -28,53 +30,83 @@ public class XposedShadowsocksPreciseRoute extends XposedBase {
 
     @Override
     protected void handleLoadPackage() throws Throwable {
-        findAndHookMethod(Resources.class, "getStringArray", int.class, new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                final int bypassPrivateRouteId = ContextUtils.getResId("bypass_private_route", "array");
-                if (bypassPrivateRouteId == (int) param.args[0]) {
-                    Context context = MyApplication.instance().createPackageContext(PackageNames.L_TWEAKS, 0);
-                    String[] routes = context.getResources().getStringArray(R.array.shadowsocks_bypass_private_route);
-                    param.setResult(routes);
-                    Logger.i("Return precise bypass_private_route, count " + routes.length);
-                }
-            }
-        });
-
         findAndHookMethod("com.github.shadowsocks.ShadowsocksVpnService", "startShadowsocksDaemon", new XC_MethodHook() {
             private Set<Unhook> hooks;
 
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                 Logger.d("startShadowsocksDaemon before");
-                hooks = hookAllMethods("scala.collection.mutable.ArrayBuffer", "$plus$eq", new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        String cmdParam = param.args[0].toString();
-                        if (cmdParam.equals("--acl")) {
-                            param.setResult(param.thisObject);
-                        } else if (cmdParam.endsWith(".acl")) {
-                            if (cmdParam.endsWith("/bypass-lan-china.acl")) {
+                if (getProfileAcl(param.thisObject).equals("bypass-lan-china")) {
+                    hooks = hookAllMethods("scala.collection.mutable.ArrayBuffer", "$plus$eq", new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            String cmdParam = param.args[0].toString();
+                            if (cmdParam.equals("--acl")) {
                                 param.setResult(param.thisObject);
-                                Logger.d("--acl " + cmdParam + " is removed.");
-                            } else {
-                                cmdParam = "--acl " + cmdParam;
-                                param.args[0] = cmdParam;
-                                Logger.d(cmdParam);
+                            } else if (cmdParam.endsWith(".acl")) {
+                                if (cmdParam.endsWith("/bypass-lan-china.acl")) {
+                                    param.setResult(param.thisObject);
+                                    Logger.i("--acl " + cmdParam + " is removed.");
+                                } else {
+                                    cmdParam = "--acl " + cmdParam;
+                                    param.args[0] = cmdParam;
+                                    Logger.i(cmdParam);
+                                }
                             }
                         }
-                    }
-                });
+                    });
+                }
             }
 
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                 Logger.d("startShadowsocksDaemon after");
-                for (Unhook hook : hooks) {
-                    hook.unhook();
+                if (hooks != null) {
+                    for (Unhook hook : hooks) {
+                        hook.unhook();
+                    }
+                    hooks = null;
                 }
-                hooks = null;
             }
         });
+
+        findAndHookMethod("com.github.shadowsocks.ShadowsocksVpnService", "startVpn", new XC_MethodHook() {
+            private Unhook hook;
+
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                Logger.d("startVpn before");
+                if (getProfileAcl(param.thisObject).equals("bypass-lan-china")) {
+                    hook = findAndHookMethod(Resources.class, "getStringArray", int.class, new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            final int bypassPrivateRouteId = ContextUtils.getResId("bypass_private_route", "array");
+                            if (bypassPrivateRouteId == (int) param.args[0]) {
+                                Context context = MyApplication.instance().createPackageContext(PackageNames.L_TWEAKS, 0);
+                                String[] routes = context.getResources().getStringArray(R.array.shadowsocks_bypass_private_route);
+                                param.setResult(routes);
+                                Logger.i("Return precise bypass_private_route, count " + routes.length);
+                            }
+                        }
+                    });
+                }
+            }
+
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                Logger.d("startVpn after");
+                if (hook != null) {
+                    hook.unhook();
+                    hook = null;
+                }
+            }
+        });
+    }
+
+    private String getProfileAcl(Object vpnService) throws Throwable {
+        Method methodProfile = vpnService.getClass().getDeclaredMethod("profile");
+        Object profile = methodProfile.invoke(vpnService);
+        Method methodRoute = profile.getClass().getDeclaredMethod("route");
+        return (String) methodRoute.invoke(profile);
     }
 }
