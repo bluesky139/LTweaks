f4f7b8822d19f3e06a517f35ea42bc4879e4be0a
bluesky139 <bluesky139@gmail.com>
2017-08-08 18:15:22 +0800

    [QQ] Optimize outer browser.

--- app/src/main/java/li/lingfeng/ltweaks/xposed/communication/XposedQQOuterBrowser.java
+++ app/src/main/java/li/lingfeng/ltweaks/xposed/communication/XposedQQOuterBrowser.java
@@ -1,7 +1,9 @@
 package li.lingfeng.ltweaks.xposed.communication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 
 import de.robv.android.xposed.XC_MethodHook;
 import li.lingfeng.ltweaks.R;
@@ -22,19 +24,25 @@ import li.lingfeng.ltweaks.xposed.XposedBase;
 }, prefs = R.string.key_qq_outer_browser)
 public class XposedQQOuterBrowser extends XposedBase {
 
-    private static final String BROWSER_ACTIVITY = "com.tencent.mobileqq.activity.QQBrowserActivity";
+    private static final String BROWSER_DELEGATED_ACTIVITY = "com.tencent.mobileqq.activity.QQBrowserDelegationActivity";
 
     @Override
     protected void handleLoadPackage() throws Throwable {
-        findAndHookActivity(BROWSER_ACTIVITY, "onCreate", Bundle.class, new XC_MethodHook() {
+        hookAllMethods(Activity.class, "startActivityForResult", new XC_MethodHook() {
             @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                Intent intent = (Intent) param.args[0];
+                if (intent.getComponent() == null
+                        || !intent.getComponent().getClassName().equals(BROWSER_DELEGATED_ACTIVITY)) {
+                    return;
+                }
+
                 Activity activity = (Activity) param.thisObject;
-                String url = activity.getIntent().getStringExtra("url");
-                if (url != null && !url.isEmpty()) {
-                    Logger.i("QQBrowserActivity url " + url);
+                String url = intent.getStringExtra("url");
+                if (Patterns.WEB_URL.matcher(url).matches()) {
+                    Logger.i("QQ url " + url);
                     ContextUtils.startBrowser(activity, url);
-                    activity.finish();
+                    param.setResult(null);
                 }
             }
         });
