af9322de9fde20bd71c5802ecd059be2651b7f6f
bluesky139 <bluesky139@gmail.com>
2017-09-04 13:43:17 +0800

    [Share] Fix "Copy to share".

--- app/src/main/java/li/lingfeng/ltweaks/utils/ShareUtils.java
+++ app/src/main/java/li/lingfeng/ltweaks/utils/ShareUtils.java
@@ -7,6 +7,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.view.View;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Created by smallville on 2017/2/18.
  */
@@ -14,20 +16,29 @@ import android.view.View;
 public class ShareUtils {
 
     public static void shareClipWithSnackbar(final Activity activity, ClipData clipData) {
-        final String text = clipData.getItemCount() > 0 ? clipData.getItemAt(0).getText().toString() : "";
-        if (text.isEmpty())
-            return;
-        SimpleSnackbar.make(activity, "你可以分享复制的内容", SimpleSnackbar.LENGTH_LONG)
-                .setAction("分享...", new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(Intent.ACTION_SEND);
-                        intent.setType("text/plain");
-                        intent.putExtra(Intent.EXTRA_TEXT, text);
-                        activity.startActivity(intent);
-                    }
-                })
-                .show();
+        try {
+            if (clipData == null) {
+                return;
+            }
+            final CharSequence text = clipData.getItemCount() > 0 ? clipData.getItemAt(0).getText() : null;
+            if (StringUtils.isEmpty(text)) {
+                return;
+            }
+            SimpleSnackbar.make(activity, "You can share copied text", SimpleSnackbar.LENGTH_LONG)
+                    .setAction("Share...", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(Intent.ACTION_SEND);
+                            intent.setType("text/plain");
+                            intent.putExtra(Intent.EXTRA_TEXT, text);
+                            activity.startActivity(intent);
+                        }
+                    })
+                    .show();
+        } catch (Throwable e) {
+            Logger.e("shareClipWithSnackbar error, " + e);
+            Logger.stackTrace(e);
+        }
     }
 
     public static void shareText(Context context, String text) {
