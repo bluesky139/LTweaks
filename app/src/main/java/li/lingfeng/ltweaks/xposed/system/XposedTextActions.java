a3e752023c813616f9f753be3a836b0c58a3d43a
bluesky139 <bluesky139@gmail.com>
2017-12-04 17:37:06 +0800

    [Text] Optimize floating menu management.

--- app/src/main/java/li/lingfeng/ltweaks/xposed/system/XposedTextActions.java
+++ app/src/main/java/li/lingfeng/ltweaks/xposed/system/XposedTextActions.java
@@ -1,5 +1,6 @@
 package li.lingfeng.ltweaks.xposed.system;
 
+import android.os.Build;
 import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,7 +18,6 @@ import li.lingfeng.ltweaks.lib.XposedLoad;
 import li.lingfeng.ltweaks.prefs.ClassNames;
 import li.lingfeng.ltweaks.prefs.PackageNames;
 import li.lingfeng.ltweaks.prefs.Prefs;
-import li.lingfeng.ltweaks.prefs.SharedPreferences;
 import li.lingfeng.ltweaks.utils.Logger;
 import li.lingfeng.ltweaks.utils.Utils;
 import li.lingfeng.ltweaks.xposed.XposedBase;
@@ -31,40 +31,34 @@ import li.lingfeng.ltweaks.xposed.XposedBase;
 public class XposedTextActions extends XposedBase {
 
     private static final String FLOATING_TOOLBAR = "com.android.internal.widget.FloatingToolbar";
-    private Map<String, Pair<Integer, Boolean>> mSavedItems;
 
     @Override
     protected void handleLoadPackage() throws Throwable {
-        Prefs.instance().registerPreferenceChangeKey(R.string.key_text_actions_set, new SharedPreferences.OnPreferenceChangeListener() {
-            @Override
-            public void onChanged(String key, Object value) {
-                mSavedItems = null;
-            }
-        });
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return;
+        }
 
         findAndHookMethod(FLOATING_TOOLBAR, "getVisibleAndEnabledMenuItems", Menu.class, new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                if (mSavedItems == null) {
-                    Set<String> savedItems = Prefs.instance().getStringSet(R.string.key_text_actions_set, null);
-                    if (savedItems == null || savedItems.size() == 0) {
-                        return;
-                    }
+                Set<String> savedItems = Prefs.instance().getStringSet(R.string.key_text_actions_set, null);
+                if (savedItems == null || savedItems.size() == 0) {
+                    return;
+                }
 
-                    mSavedItems = new HashMap<>(savedItems.size());
-                    for (String item : savedItems) {
-                        String[] strs = Utils.splitMax(item, ':', 4);
-                        String name = strs[3];
-                        int order = Integer.parseInt(strs[0]);
-                        boolean block = Boolean.parseBoolean(strs[1]);
-                        mSavedItems.put(name, Pair.create(order, block));
-                    }
+                final Map<String, Pair<Integer, Boolean>> savedItemMap = new HashMap<>(savedItems.size());
+                for (String savedItem : savedItems) {
+                    String[] strs = Utils.splitMax(savedItem, ':', 4);
+                    String name = strs[3];
+                    int order = Integer.parseInt(strs[0]);
+                    boolean block = Boolean.parseBoolean(strs[1]);
+                    savedItemMap.put(name, Pair.create(order, block));
                 }
 
                 List<MenuItem> items = (List<MenuItem>) param.getResult();
                 for (int i = items.size() - 1; i >= 0; --i) {
                     MenuItem item = items.get(i);
-                    Pair<Integer, Boolean> pair = mSavedItems.get(item.getTitle());
+                    Pair<Integer, Boolean> pair = savedItemMap.get(item.getTitle());
                     if (pair != null && pair.second) {
                         Logger.d("Remove floating menu " + item.getTitle());
                         items.remove(i);
@@ -75,9 +69,9 @@ public class XposedTextActions extends XposedBase {
                 Collections.sort(items, new Comparator<MenuItem>() {
                     @Override
                     public int compare(MenuItem i1, MenuItem i2) {
-                        Pair<Integer, Boolean> pair = mSavedItems.get(i1.getTitle());
+                        Pair<Integer, Boolean> pair = savedItemMap.get(i1.getTitle());
                         Integer order1 = pair == null ? null : pair.first;
-                        pair = mSavedItems.get(i2.getTitle());
+                        pair = savedItemMap.get(i2.getTitle());
                         Integer order2 = pair == null ? null : pair.first;
                         if (order1 == null && order2 == null) {
                             return 0;
