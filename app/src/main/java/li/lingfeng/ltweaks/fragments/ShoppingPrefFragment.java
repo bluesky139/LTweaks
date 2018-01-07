fb48e892e09e555424bf233c8df7a2217a3aaad7
bluesky139 <bluesky139@gmail.com>
2018-01-07 18:56:24 +0800

    Remove JD link to app.

--- app/src/main/java/li/lingfeng/ltweaks/fragments/ShoppingPrefFragment.java
+++ app/src/main/java/li/lingfeng/ltweaks/fragments/ShoppingPrefFragment.java
@@ -1,17 +1,12 @@
 package li.lingfeng.ltweaks.fragments;
 
-import android.content.ComponentName;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.Preference;
 
 import li.lingfeng.ltweaks.R;
-import li.lingfeng.ltweaks.activities.JDActivity;
 import li.lingfeng.ltweaks.activities.JDHistoryActivity;
 import li.lingfeng.ltweaks.lib.PreferenceChange;
-import li.lingfeng.ltweaks.prefs.Prefs;
 import li.lingfeng.ltweaks.utils.ComponentUtils;
-import li.lingfeng.ltweaks.utils.Logger;
 
 /**
  * Created by smallville on 2016/12/25.
@@ -24,14 +19,8 @@ public class ShoppingPrefFragment extends BasePrefFragment {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.pref_shopping);
 
-        uncheckPreferenceByDisabledComponent(R.string.key_jd_open_link_in_app, JDActivity.class);
         uncheckPreferenceByDisabledComponent(R.string.key_jd_history, JDHistoryActivity.class);
         uncheckPreferenceByDisabledComponent(R.string.key_suning_history, JDHistoryActivity.class);
-    }
-
-    @PreferenceChange(prefs = R.string.key_jd_open_link_in_app)
-    private void enableJdOpenLinkInApp(Preference preference, boolean enabled) {
-        ComponentUtils.enableComponent(JDActivity.class, enabled);
     }
 
     @PreferenceChange(prefs = { R.string.key_jd_history, R.string.key_suning_history })
